50c91ba21feeee55310e95ebeac116a3480e3abe
Keerthana T <Keerthana T@DESKTOP-4T2F8OK>
2023-09-19 09:42:21 +0530

    First

---
+++ FirstProjectt/src/ForLoop.java
@@ -0,0 +1,12 @@
+
+public class ForLoop {
+
+	public static void main(String[] args) {
+		int i;
+		for(i=0;i<=10;i++)
+			System.out.println("value of i:"+i);
+		// TODO Auto-generated method stub
+
+	}
+
+}
