6e25c15ef4861852c3d550cb00ecd21805086132
Keerthana T <Keerthana T@DESKTOP-4T2F8OK>
2023-10-05 06:08:49 +0530

    Wrapper

---
+++ WrapperClass/src/com/cg/wrapperclass/WrapperClass.java
@@ -0,0 +1,22 @@
+package com.cg.wrapperclass;
+
+public class WrapperClass {
+	
+	//1.Autoboxing 2.unboxing
+	
+	//Autoboxing
+	int i=10; //Primitive
+	Integer k=i; //Wrapper 
+
+	//unboxing
+	int x=10;
+	int y=x;
+	
+	//Typecasting
+	float a=12.5f;
+	Integer b=(int)a;
+	
+	
+	
+	
+}
